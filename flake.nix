b3bae8898294c36b0c006b755b7c46a88f993b29
Daniil Kharitonov <daniil.kharitonov@student.hs-anhalt.de>
2024-01-09 23:41:56 +0100

    add project environment

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        poetry2nix = inputs.poetry2nix.lib.mkPoetry2Nix { inherit pkgs; };
+        poetryEnv = poetry2nix.mkPoetryEnv {
+          projectDir = ./.;
+          # editablePackageSources = {
+          #   audio_analyzer = ./audio_analyzer;
+          # };
+          python = pkgs.python311;
+          extraPackages = (ps: with ps; [ tkinter ]);
+          overrides = poetry2nix.defaultPoetryOverrides.extend (self: super: {
+            matplotlib = super.matplotlib.overrideAttrs (
+              old: { enableTk = true; }
+            );
+            tk = super.tk.overridePythonAttrs
+              (
+                old: { buildInputs = (old.buildInputs or [ ]) ++ [ super.setuptools ]; }
+              );
+            audioread = super.audioread.overridePythonAttrs (
+              old: { buildInputs = (old.buildInputs or [ ]) ++ [ super.flit-core ]; }
+            );
+            soxr = super.soxr.overridePythonAttrs (
+              old: { buildInputs = (old.buildInputs or [ ]) ++ [ super.cython ]; }
+            );
+          });
+        };
+      in
+      rec {
+        devShells.default = pkgs.mkShell rec {
+          packages = with pkgs; [
+            poetry
+          ];
+          buildInputs = with pkgs; [
+            poetryEnv
+            portaudio
+          ];
+          shellHook = ''
+            export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath buildInputs}:$LD_LIBRARY_PATH"
+            alias pyright='poetry run -- pyright'
+            alias pyright-langserver='poetry run -- pyright-langserver'
+            export PYTHONPATH="$PWD"
+          '';
+        };
+      });
+}
